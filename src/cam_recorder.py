7e85af9ac187041b6cfd05e452333c52d2942ce3
bijanmehr <bijanmehralizadeh@gmail.com>
2019-09-19 17:18:35 +0430

    modify video_recorder calling

--- src/cam_recorder.py
+++ src/cam_recorder.py
@@ -7,11 +7,10 @@ from sensor_msgs.msg import CameraInfo
 
 import time
 import os
+import signal
 from os.path import expanduser
 import datetime
 import subprocess
-
-
 
 
 writer_flag = False
@@ -21,6 +20,9 @@ first_seq2 = -1
 current_frame2 = 0
 cam1_subprocess = None
 cam2_subprocess = None
+
+cam_subprocess = None
+
 dir = None
 end_process = True
 stage = ''
@@ -72,31 +74,51 @@ def set_dir(data):
 
 
 def turn_on_cams(name):
-    global cam1_subprocess,cam2_subprocess,dir
+    global cam1_subprocess,cam2_subprocess,dir,cam_subprocess
 
     if writer_flag == True:
-        if cam1_subprocess is None:
-            print('Start recording cam1')
-            cam1_subprocess = subprocess.Popen(["rosrun", "image_view","video_recorder",
-                "image:=/camera1/image_raw","_filename:=\"%s/%s_cam1.avi\""%(dir,name),"_fps:=30"])
+        # if cam1_subprocess is None:
+        #     print('Start recording cam1')
+        #     # filename_cam1 = "_filename:=\"%s/%s_cam1.avi\""%(dir,name)
+        #     filename_cam1 = "\"%s/%s_cam1.avi\""%(dir,name)
+        #     cmd1 = "roslaunch cabinet cam1_recorder.launch filename_cam1:=%s" %(filename_cam1)
+        #     cam1_subprocess = subprocess.Popen(cmd1,shell=True)
+        #     # cam1_subprocess = subprocess.Popen(["rosrun", "image_view","video_recorder",
+        #     #     "image:=/camera1/image_raw",filename_cam1,"_fps:=30"],shell=True)
         
-        if cam2_subprocess is None:
-            print('Start recording cam2')
-            cam2_subprocess = subprocess.Popen(["rosrun", "image_view","video_recorder",
-                "image:=/camera2/image_raw","_filename:=\"%s/%s_cam2.avi\""%(dir,name),"_fps:=30"])
+        # if cam2_subprocess is None:
+        #     print('Start recording cam2')
+        #     # filename_cam2 = "_filename:=\"%s/%s_cam2.avi\""%(dir,name)
+        #     filename_cam2 = "\"%s/%s_cam2.avi\""%(dir,name)
+        #     cmd2 = "roslaunch cabinet cam1_recorder.launch filename_cam2:=%s" %(filename_cam2)
+        #     cam2_subprocess = subprocess.Popen(cmd2,shell=True)
+        #     # cam2_subprocess = subprocess.Popen(["rosrun", "image_view","video_recorder",
+        #     #     "image:=/camera2/image_raw",filename_cam2,"_fps:=30"],shell=True)
 
+        if cam1_subprocess is None and cam2_subprocess is None:
+            print("start recording")
+            filename_cam1 = "\"%s/%s_cam1.avi\""%(dir,name)
+            filename_cam2 = "\"%s/%s_cam2.avi\""%(dir,name)
+            cmd = "roslaunch cabinet camera_recorder.launch filename_cam1:=%s filename_cam2:=%s" %(filename_cam1,filename_cam2)
+            cam_subprocess = subprocess.Popen(cmd,shell=True)
 
 def turn_off_cams():
-    global cam1_subprocess,cam2_subprocess
-    if cam1_subprocess is not None:
-        print('Stop recording')
-        cam1_subprocess.terminate()
-        cam1_subprocess = None
+    global cam1_subprocess,cam2_subprocess,cam_subprocess
+    # if cam1_subprocess is not None:
+    #     print('Stop recording')
+    #     cam1_subprocess.terminate()
+    #     cam1_subprocess = None
 
-    if cam2_subprocess is not None:
-        print('Stop recording')
-        cam2_subprocess.terminate()
-        cam2_subprocess = None
+    # if cam2_subprocess is not None:
+    #     print('Stop recording')
+    #     cam2_subprocess.terminate()
+    #     cam2_subprocess = None
+    if cam_subprocess is not None:
+        print("stop recording")
+        # cam_subprocess.terminate()
+        os.system("rosnode kill /cam1_recorder /cam2_recorder")
+        # os.killpg(os.getpgid(cam_subprocess.pid), signal.SIGTERM)
+        # os.killpg(os.getpgid(cam_subprocess.pid), signal.SIGTERM)
 
 
 def start_end_logging(data):
@@ -119,6 +141,9 @@ def start_end_logging(data):
         turn_on_cams('start_parrot')
         end_process = False
 
+    elif data.data == 'stop_test':
+        os.system("rosnode kill /cam1_recorder /cam2_recorder")
+
     else:
         #terminate recording
         turn_off_cams()
@@ -128,7 +153,7 @@ def start_end_logging(data):
 def main():
     global data_writer
 
-    rospy.init_node('cam_recorder')
+    rospy.init_node('cam_logger')
     # Define your image topic
     
     rospy.Subscriber('web/parrot_command', String, set_action)
